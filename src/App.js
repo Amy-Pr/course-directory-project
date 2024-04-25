e839bd9ea99418f3182b09dfe3e48a41dd6a1834
amy_pr <ampreci@gmail.com>
2024-04-24 17:35:18 -0700

    import react Router components and add pages to root

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Header from "./components/Header";
+import Teachers from "./components/Teachers";
+import Courses from "./components/Courses"
 
 function App() {
   return (
     <div className="container">
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+      {/* Forward slash indicates root path */}
+      <Route path="about" element={<About />} />
+      <Route path="teachers" element={<Teachers />} />
+      <Route path="courses" element={<Courses />} />
+    </Routes>
 
     </div>
   );
