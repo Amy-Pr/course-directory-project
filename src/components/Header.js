9f5a6444870605ca9f952ad67dbc475484c9ab09
amy_pr <ampreci@gmail.com>
2024-04-25 14:33:48 -0700

    change Link to NavLink with example of styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="about">About</Link></li>
-      <li><Link to="teachers">Teachers</Link></li>
-      <li><Link to="courses">Courses</Link></li>
+      {/* There is an isActive property in the NavLink, and you can use it to change styles and class names inline.
+      <li><NavLink to="/" style={({isActive}) => isActive ? {background: 'tomato'} : undefined} >Home</NavLink></li>
+      <li><NavLink to="about" className={({isActive}) => isActive ? "custom-class-name" : undefined}>About</NavLink></li> */}
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="about">About</NavLink></li>
+      <li><NavLink to="teachers">Teachers</NavLink></li>
+      <li><NavLink to="courses">Courses</NavLink></li>
     </ul>    
   </header>
 );
