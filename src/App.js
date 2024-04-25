a709fd8b03db76672ef4e7fa171f9e41bcb22c03
amy_pr <ampreci@gmail.com>
2024-04-25 14:14:13 -0700

    add Link component to navigation links and set to properties

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Header from "./components/Header";
 import Teachers from "./components/Teachers";
-import Courses from "./components/Courses"
+import Courses from "./components/Courses";
 
 function App() {
   return (
